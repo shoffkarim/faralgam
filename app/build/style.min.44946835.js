8aec1e1d8ad1ac62a6cc732f38303aea9a6994b2
shoffkarim <k4r1mch1k@gmail.com>
2021-06-02 13:57:11 +0500

    стили для шапки

--- app/build/style.min.44946835.js
+++ app/build/style.min.44946835.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58176" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58889" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.min.44946835.js.map
+//# sourceMappingURL=style.min.44946835.js.map
