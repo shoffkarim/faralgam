92e3cacf055ec83852b115db78d54a0961856c41
shoffkarim <k4r1mch1k@gmail.com>
2021-06-03 08:54:10 +0500

    верстка секции магазинов

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,13 @@
+/* eslint-disable no-undef */
 // STYLES=============================================================================
 import '../sass/style.sass';
+
+// eslint-disable-next-line no-unused-vars
+let shopsSlider = new Swiper(".shops-slider", {
+  slidesPerView: 4,
+  spaceBetween: 35,
+  navigation: {
+    nextEl: ".shops-slider-next",
+    prevEl: ".shops-slider-prev",
+  },
+});
