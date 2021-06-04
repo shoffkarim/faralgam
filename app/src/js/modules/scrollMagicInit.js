f80d2e090ff9e67c48e261cbaa4dc103e652d5b4
shoffkarim <k4r1mch1k@gmail.com>
2021-06-04 16:59:28 +0500

    часть анимации блока с галерей

--- app/src/js/modules/scrollMagicInit.js
+++ app/src/js/modules/scrollMagicInit.js
@@ -12,24 +12,30 @@ function scrollMagic() {
     let scenes = [];
     let cloudAnimation1 = new TimelineMax();
     cloudAnimation1
-    .to(".gallery-column--1", 50, { x: "-= 100%" }, 0)
-    .to(".gallery-column--2", 150, { x: "-= 250%" }, 0)
-    .to(".gallery-column--4", 150, { x: "+= 250%" }, 0)
-    .to(".gallery-column--5", 50, { x: "+= 100%" }, 0);
+    .to(".gallery-column--1", 50, { x: "-100%", ease: Power1.easeOut }, 0)
+    .to(".gallery-column--2", 150, { x: "-250%", ease: Power1.easeOut }, 0)
+    .to(".gallery-column--4", 150, { x: "250%", ease: Power1.easeOut }, 0)
+    .to(".gallery-column--5", 50, { x: "100%", ease: Power1.easeOut }, 0);
+    // .to(".gallery-column--3", 150, {
+    //   width: "100%",
+    //   ease: Circ.easeOut,
+    // }, 0);
+    // TweenMax.set(".gallery-column--3", { transformOrigin: "0% 0%" });
     scenes.push(
       new ScrollMagic.Scene({
         triggerElement: ".js-gallery",
-        duration: "50%",
+        duration: "100%",
         triggerHook: 0,
-        offset: 200,
+        offset: 0,
       })
         .setTween(cloudAnimation1)
+        .setClassToggle(".gallery-column--3", "top")
         .addTo(controller)
     );
     scenes.push(
       new ScrollMagic.Scene({
         triggerElement: ".gallery-wrapper",
-        duration: "100%",
+        duration: "120%",
         triggerHook: 0,
         offset: 0,
       })
